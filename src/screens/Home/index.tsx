ca4bea1ee872bd8efaa01bbd32d4f19a59c3b68f
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2023-10-11 14:18:55 -0300

    feat: ensure attendees are added to the list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   Text, 
   View, 
   TextInput, 
   TouchableOpacity, 
-  FlatList 
+  FlatList,
+  Alert
 } from 'react-native';
 
 import { styles} from './styles';
 import { Participant } from '../../components/Participant';
 
-export function Home() {
-  const participants = ['Eleazar', 'Erick', 'Ricardo', 'Will', 'Luiz', 'Cristian', 'Matheus', 'James Tiberius Quito', 'Margô', 'Alfredo'];
+const ACTUAL_DATE = new Date();
 
-  const handleParticipantAdd = () => {
-    console.log('adicionou')
+export function Home() {
+  const [participants, setParticipants] = useState<Array<string>>([]);
+  const [participantName, setParticipantName] = useState<string>('');
+
+  const handleParticipantAdd = (name: string) => {
+    if(participants.includes(name)) {
+      return Alert.alert('Participante Existe', 'Já existe um participante na lista com esse nome.')
+    }
+    setParticipants((prevState) => [...prevState, name]);
   }
   
-  const handleParticipantRemove = () => {
-    console.log('removeu')
+  const handleParticipantRemove = (name: string) => {
+    Alert.alert('Remover', `Remover o participante ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => {
+          handleRemove(name);
+          Alert.alert("Deletado!");
+        }
+      },
+      {
+        text: "Não",
+        style: 'cancel'
+      }
+    ]);
   };
 
+  const handleRemove = (name: string) => {
+    participants.find((participant: string, key) => {
+      if(participant === name) {
+        participants.splice(key, 1);
+      }
+    });
+  }
+  
   return (
     <View style={styles.container}>
     <Text style={styles.title}>Nome do Evento</Text>
 
     <Text style={styles.text}>
-      Sexta, 4 de Novembro de 2022
+      {Intl.DateTimeFormat('pt-br', {
+        dateStyle: 'full',
+      }).format(ACTUAL_DATE)}
     </Text>
 
     <View style={styles.form}>
@@ -34,9 +63,15 @@ export function Home() {
       style={styles.input}
       placeholder='Nome do Participante'
       placeholderTextColor="#6B6B6B"
-      />
+      onChangeText={setParticipantName}
+    />
 
-    <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+    <TouchableOpacity 
+      style={styles.button} 
+      onPress={
+        () => 
+        handleParticipantAdd(participantName.trim())
+      }>
       <Text style={styles.buttonText}>
         +
       </Text>
@@ -56,12 +91,11 @@ export function Home() {
         <Participant 
         key={item}
         name={item} 
-        onRemove={handleParticipantRemove} 
+        onRemove={() => handleParticipantRemove(item)} 
         />
       )}
       
     />
-
   </View>
   );
 }
